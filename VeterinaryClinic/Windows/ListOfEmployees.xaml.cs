79d887f0bcaa88d7ac5a123673103c6f319398e4
galim <galim@LAPTOP-RTC42L80>
2022-04-09 15:44:33 +0300

    Добавьте файлы проекта.

---
+++ VeterinaryClinic/Windows/ListOfEmployees.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VeterinaryClinic.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ListOfEmployees.xaml
+    /// </summary>
+    public partial class ListOfEmployees : Window
+    {
+        public ListOfEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ VeterinaryClinic/Windows/MainAdmin Window.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VeterinaryClinic.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainAdmin_Window.xaml
+    /// </summary>
+    public partial class MainAdmin_Window : Window
+    {
+        public MainAdmin_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
